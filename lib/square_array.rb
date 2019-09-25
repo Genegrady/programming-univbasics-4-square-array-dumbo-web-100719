5582a8b4c7a90a5e8c8eb865da2cc525f300e5a5
Eugene Grady <grady.gene.thomas@gmail.com>
2019-09-25 17:18:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,8 +2,8 @@ def square_array(array)
  i = 0 
  array2= Array.new(numbers.size)
  while i < array.length 
-   array2[i]= array**2
-   counter += 1 
+   array2[i]= array[i]**2
+   i += 1 
  end
   return array2
 end
